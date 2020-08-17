7e5840b1aa62534dd581e40bd93c8cfc4df25ec7
Viacheslav Mefodin <mefodin.v@gmail.com>
2020-08-17 09:19:50 +0300

    Use dynamic matchers in NodePattern and all cops

--- lib/rubocop/cop/rspec/empty_example_group.rb
+++ lib/rubocop/cop/rspec/empty_example_group.rb
@@ -76,7 +76,7 @@ module RuboCop
         #   @param node [RuboCop::AST::Node]
         #   @yield [RuboCop::AST::Node] example group body
         def_node_matcher :example_group_body, <<~PATTERN
-          (block #{ExampleGroups::ALL.send_pattern} args $_)
+          (block #{send_pattern('#rspec_all_example_groups')} args $_)
         PATTERN
 
         # @!method example_or_group_or_include?(node)
@@ -95,11 +95,12 @@ module RuboCop
         #   @return [Array<RuboCop::AST::Node>] matching nodes
         def_node_matcher :example_or_group_or_include?, <<~PATTERN
           {
-            #{Examples::ALL.send_pattern}
-            #{Examples::ALL.block_pattern}
-            #{ExampleGroups::ALL.block_pattern}
-            #{Includes::ALL.send_pattern}
-            #{Includes::ALL.block_pattern}
+            #{block_pattern(
+              '{#rspec_all_examples '\
+              '#rspec_all_example_groups '\
+              '#rspec_all_includes}'
+            )}
+            #{send_pattern('{#rspec_all_examples #rspec_all_includes}')}
             (send nil? #custom_include? ...)
           }
         PATTERN
@@ -120,7 +121,7 @@ module RuboCop
         #   @param node [RuboCop::AST::Node]
         #   @return [Array<RuboCop::AST::Node>] matching nodes
         def_node_matcher :examples_inside_block?, <<~PATTERN
-          (block !#{Hooks::ALL.send_pattern} _ #examples?)
+          (block !#{send_pattern('#rspec_hooks')} _ #examples?)
         PATTERN
 
         # @!method examples_directly_or_in_block?(node)
