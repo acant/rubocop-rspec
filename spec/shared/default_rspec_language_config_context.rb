a17db012ac721e0e19bca3af19e5a4c5662c253e
Viacheslav Mefodin <mefodin.v@gmail.com>
2020-08-17 09:34:03 +0300

    Use config context
    * Add shared context with default RSpec language definitions
    * Use it for all cop specs
    * Remove redundant config tags for cop specs
    * Remove `cop` subjects so they are conflicting with the ones from
      shared context.
    * Use config context for language element specs

---
+++ spec/shared/default_rspec_language_config_context.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+RSpec.shared_context 'with default AllCops.RSpec.Language config', :config do
+  # Deep duplication is needed to prevent config leakage between examples.
+  let(:all_cops_config) do
+    cfg = { 'TargetRubyVersion' => ruby_version }
+    cfg['TargetRailsVersion'] = rails_version if rails_version
+    default_language = RuboCop::ConfigLoader
+      .default_configuration
+      .for_all_cops.dig('RSpec', 'Language')
+    cfg.merge('RSpec' => { 'Language' => deep_dup(default_language) })
+  end
+
+  def deep_dup(object)
+    case object
+    when Array
+      object.map { |item| deep_dup(item) }
+    when Hash
+      object.transform_values(&method(:deep_dup))
+    else
+      object # only collections undergo modifications and need duping
+    end
+  end
+end
